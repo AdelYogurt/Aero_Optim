d554d9c95b3e79f92ec09bf59aaf8c690a64394c
Adel_Yogurt <1415071309@qq.com>
2023-08-04 20:28:49 +0800

    delete useless file

--- Airfoil/test.py
+++
@@ -1,8 +0,0 @@
-from model.util import MyLogger
-
-
-if __name__ == '__main__':
-    my_logger1=MyLogger(log_filename='test1.log')
-    my_logger2=MyLogger(log_filename='test2.log')
-    my_logger1.logger.info('1')
-    my_logger2.logger.info('2')
