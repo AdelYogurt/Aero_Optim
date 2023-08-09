9687cdf78c51527f1c6d1b021b21beb84a4d46b3
Adel_Yogurt <1415071309@qq.com>
2023-08-09 10:48:05 +0800

    delete useless file

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import Popen, PIPE
-
-if __name__ == "__main__":
-    process = Popen("SU2_CFD", stdin=PIPE, stdout=PIPE,
-                    stderr=PIPE)
-    stdout, stderr = process.communicate()  # notice: donot use wait()!!!
-    print(stderr)
