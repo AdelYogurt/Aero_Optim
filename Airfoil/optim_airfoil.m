e60e4dfef5b0073a50c986366a37665e6ac9a0e5
Adel_Yogurt <1415071309@qq.com>
2023-12-31 14:31:54 +0800

    updata Airfoil

--- Airfoil/optim_airfoil.m
+++ Airfoil/optim_airfoil.m
@@ -2,6 +2,8 @@ clc;
 clear;
 close all hidden;
 
+%% PATH
+
 % cd ..
 % addpath cgns4m;
 % addpath coord;
@@ -22,24 +24,24 @@ out_logger=[];
 
 % geometry module
 % geom_param.solver='HICKS_HENNE';
-% geom_param.mesh_point=load('mesh_data_airfoil.mat','mesh_point').mesh_point;
+% geom_param.mesh_point=load('mesh/mesh_data_airfoil.mat','mesh_point').mesh_point;
 % hicks_hemme_define.MARKER=[repmat({'AIRFOIL_LOW'},5,1);repmat({'AIRFOIL_UP'},5,1)];
 % hicks_hemme_define.PARAM=[[zeros(5,1),(1/6:1/6:5/6)'];[ones(5,1),(1/6:1/6:5/6)']];
 % geom_param.hicks_hemme_define=hicks_hemme_define;
 
 geom_param.solver='CST';
-geom_param.mesh_coord=load('mesh_data_airfoil.mat','mesh_coord').mesh_coord;
+geom_param.mesh_coord=load('mesh/mesh_data_airfoil.mat','mesh_coord').mesh_coord;
 
 % mesh module
 mesh_param.solver='SU2_DEF';
-mesh_param.initial_mesh_filestr='Fluent/NACA0012.cgns';
+mesh_param.initial_mesh_filestr='mesh/NACA0012.cgns';
 mesh_param.SU2_DEF_param='SU2/NACA0012_deform.cfg';
 mesh_param.dat_filestr='geom/NACA0012_deform.dat';
 mesh_param.mesh_filestr='geom/airfoil.su2';
 
 % CFD module
 CFD_param.solver='Fluent';
-CFD_param.fluent_jou_filestr='Fluent/airfoil.jou';
+CFD_param.fluent_jou_filestr='Fluent/airfoil_80_2.jou';
 CFD_param.fluent_dir='';
 CFD_param.solver_dimension=2;
 CFD_param.out_filename='fluent_history.out';
@@ -48,8 +50,10 @@ CFD_param.out_filename='fluent_history.out';
 % CFD_param.SU2_CFD_param='SU2/airfoil.cfg';
 
 % model=AirfoilModel(partitions,geom_param,mesh_param,CFD_param,[],[],run_desc,out_logger);
-% geo_in.up=[linspace(0,1,5)',rand(5,1)*0.1];
-% geo_in.low=[linspace(0,1,5)',-rand(5,1)*0.1];
+% geo_in.C_par_low=[0.5,1.0];
+% geo_in.Poles_low=importdata('geom/RAE2822_CSTshape_low.txt');
+% geo_in.C_par_up=[0.5,1.0];
+% geo_in.Poles_up=importdata('geom/RAE2822_CSTshape_up.txt');
 % [geo_out,mesh_out,CFD_out]=model.solveAirfoil(geo_in);
 
 problem=AirfoilProblem(partitions,geom_param,mesh_param,CFD_param,[],[],run_desc,out_logger);
